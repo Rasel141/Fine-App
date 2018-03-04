b875b66aa531462fd913703c4a024b4232684561
Rasel <rrasel141@gmail.com>
2018-03-05 01:46:12 +0600

    completed smooth scroling and smooth navbar

--- js/script.js
+++ js/script.js
@@ -57,3 +57,39 @@ $('.section-clients__list').owlCarousel({
     }
 });
 
+
+/*=================================================
+                    NAVIGATION
+===================================================*/
+// show/hide transparent black navigation
+$(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 50) {
+            // hide nav
+            $("nav").removeClass("header-nav");
+            
+            // $("#back-to-top").fadeOut();
+        } else {
+            // show nav
+            $("nav").addClass("header-nav");
+            // $("#back-to-top").fadeIn();
+        }
+    });
+
+});
+
+// Smooth scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+
+        var section = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    });
+
+});
